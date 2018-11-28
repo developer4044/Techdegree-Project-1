25cfe5f2126cb4bf75f81dfbac8ad10a69194b1a
Moath <developer4044@gmail.com>
2018-11-28 21:27:43 +0200

    Code optimizations

--- number_guessing.py
+++ number_guessing.py
@@ -11,11 +11,6 @@ the_number = get_new_secret_number()
 number_found = False
 user_tries = 0
 
-print("I have a number in my mind, can you guess what is the number?")
-print("Challenge accepted! Pick a number between 1 and 100")
-
-
-
 
 # Give the player some hint!
 def print_hint(user_number):
@@ -26,25 +21,42 @@ def print_hint(user_number):
         print("Try higher")
 
 
-while not number_found:
-    user_answer = -1
-    try:
-        user_input = input("Guess a number: ")
-        user_answer = int(user_input)
-        if user_answer != the_number:
-            print_hint(user_answer)
-            user_tries += 1
+def stop_game():
+    global number_found
+    number_found = True
 
-        else:
-            print("{} is the number. You found it. after {} tries.".format(the_number, user_tries))
-            play_again = input("Play again? Y/N ")
-            if play_again.upper() == 'Y':
-                the_number = get_new_secret_number()
-                continue
+
+def start_game():
+    global number_found
+    global the_number
+    global user_tries
+    number_found = False
+
+    print("I have a number in my mind, can you guess what is the number?")
+    print("Challenge accepted! Pick a number between 1 and 100")
+
+    while not number_found:
+        user_answer = -1
+        try:
+            user_input = input("Guess a number: ")
+            user_answer = int(user_input)
+            if user_answer != the_number:
+                print_hint(user_answer)
+                user_tries += 1
+
             else:
-                print("Good Bye!")
-                break
+                print("{} is the number. You found it. after {} tries.".format(the_number, user_tries))
+                play_again = input("Play again? Y/N ")
+                if play_again.upper() == 'Y':
+                    the_number = get_new_secret_number()
+                    continue
+                else:
+                    print("Good Bye!")
+                    stop_game()
 
-    except ValueError:
-        print("Oh,Not that way :D, try again")
-        continue
+        except ValueError:
+            print("Oh,Not that way :D, try again")
+            continue
+
+
+start_game()
